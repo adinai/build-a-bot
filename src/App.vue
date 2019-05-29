7179a55bed485f9c11545c8e1fcb76a705313a5e
Adina Kaiymova <adina.kaiymova@gmail.com>
2019-05-29 12:30:02 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img class="logo" src="./assets/build-a-bot-logo.png"/>
+            Buil-a-Bot
+            </li>
+        </ul>
+
+      </nav>
+    </header>
+    <main>
+      <home-page/>
+    </main>
+  </div>
+</template>
+
+<script>
+import HomePage from './home/HomePage.vue';
+
+export default {
+  name: 'app',
+  components: {
+    HomePage,
+  },
+};
+</script>
+
+<style>
+body {
+  background: linear-gradient(to bottom, rgb(16, 3, 73), rgb(24, 71, 133));
+  background-attachment: fixed;
+}
+main {
+margin: 0 auto;
+padding: 30px;
+background-color: antiquewhite;
+width: 1024px;
+min-height: 300px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+header {
+background-color: white;
+width: 1084px;
+margin: 0 auto;
+}
+
+ul{
+  padding: 3px;
+  display: flex;
+}
+
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+
+.logo{
+  vertical-align: middle;
+  height: 30px;
+}
+</style>
---
+++ src/build/RobotBuilder.vue
@@ -0,0 +1,16 @@
+<template>
+    
+
+</template>
+
+
+<script>
+export default {
+    name: 'RobotBuilder'
+    
+}
+</script>
+
+<style>
+
+</style>
